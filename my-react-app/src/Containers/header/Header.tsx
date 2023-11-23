bed7d1f20db30c7f38d3b1d8c74e9f86e9963e82
Vikas Lavwanshi <vikas.lavwanshi@postgrad.curtin.edu.au>
2023-11-23 15:34:29 +0800

    added header code and completed navbar code

--- my-react-app/src/Containers/header/Header.tsx
+++ my-react-app/src/Containers/header/Header.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import people from '../../Assets/Group 81.png';
+import ai from '../../Assets/Shop.png';
 import './header.css';
 
-const Header = () => {
-  return (
-    <div>
-        Header
-    </div>
-  )
-}
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Let&apos;s make some Custom T-shirt for your Loved one&apos;s</h1>
+      <p>Here, you bring Ideas and Pictures to Life !! , We help you build your favorite Custom merchandise T-shirt's and accessories. Visit Store for offline orders.</p>
 
-export default Header
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+  </div>
+);
+
+export default Header;
